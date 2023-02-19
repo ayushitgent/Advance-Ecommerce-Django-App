80dd252fd62cb954b952eff5eb38a9d02c7f6372
Ayush <ayushitgent@gmail.com>
2023-02-20 00:36:28 +0530

    commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' #'smtpout.secureserver.net'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 587 
 EMAIL_HOST_USER = "iayushguptastudent@gmail.com"
-EMAIL_HOST_PASSWORD = 
+# EMAIL_HOST_PASSWORD = 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
